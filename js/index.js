2a11bd8b02d7a060873d4ab62d09c2145f9a3f96
GangHo <tiger452003@gmail.com>
2021-03-08 22:22:05 +0900

    XMLHttpRequest 를 활용한 비동기 통신 로그인(JQuery free)

---
+++ js/index.js
@@ -0,0 +1,30 @@
+function ajax(e) {
+    e.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+
+    var email = document.getElementById("emailAddress").value;
+    var password = document.getElementById("password").value;
+    var data = {
+        "email" : email,
+        "password" : password 
+    };
+
+    xhr.onreadystatechange = function(data) {
+        if(xhr.readyState == xhr.DONE) {
+            if(xhr.status == 200) {
+                console.log(xhr.responseText);
+                alert(email + " Login success");
+            }else {
+                console.error(xhr.responseText);
+            }
+        }
+    };
+
+    xhr.open("POST","http://localhost:8080/login");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+
+    
+
+}
