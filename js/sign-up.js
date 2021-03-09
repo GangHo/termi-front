e1530226a046cddc3e6a779301c5320cfe90de90
GangHo <tiger452003@gmail.com>
2021-03-09 23:49:41 +0900

    XMLHttpRequest 를 활용한 비동기 통신 Sign Up(JQuery free)

---
+++ js/sign-up.js
@@ -0,0 +1,33 @@
+function ajax(e) {
+    e.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("emailAddress").value;
+    var password = document.getElementById("password").value;
+    
+    var data = {
+        "name" : name,
+        "email" : email,
+        "password" : password 
+    };
+
+    xhr.onreadystatechange = function(data) {
+        if(xhr.readyState == xhr.DONE) {
+            if(xhr.status == 200) {
+                console.log(xhr.responseText);
+                alert(email + " Join success");
+            }else {
+                console.error(xhr.responseText);
+            }
+        }
+    };
+
+    xhr.open("POST","http://localhost:8080/members");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+
+    
+
+}
